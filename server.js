2e7dfd2bc539beac4538e2faae4c02c6a28d6848
Kamel <kamelo68@gmail.com>
2024-01-31 17:06:28 +0000

    added checkbox options feature

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 const hostname = 'localhost';
+const messageMedium = [{ value: "Email", label: "Email" }, { value: "Push", label: "Push" }]
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); // To serve static files like CSS
 
 app.get('/', (req, res) => {
-    res.render('pages/index', { response: null, formData: {} });
+    res.render('pages/index', { response: null, formData: {}, messageMedium: messageMedium, selectedMediumOptions: [] });
 });
 
 app.post('/submit', (req, res) => {
+    console.log(`req.body: ${JSON.stringify(req.body)}`);
     const { input1, input2, date1, date2 } = req.body;
+    // Capture the selected options from the form data
+    const selectedMediumOptions = req.body.messageMedium || [];
 
     // Handle the form data as needed
     console.log('Form data received:');
@@ -28,7 +32,7 @@ app.post('/submit', (req, res) => {
     setTimeout(() => {
         // Render the response page with the response message and form data
         const responseMessage = 'Form submitted successfully!';
-        res.render('pages/index', { response: responseMessage, formData: { input1, input2, date1, date2 } });
+        res.render('pages/index', { response: responseMessage, formData: { input1, input2, date1, date2 }, messageMedium: messageMedium, selectedMediumOptions: selectedMediumOptions });
     }, 1000);
 });
 
